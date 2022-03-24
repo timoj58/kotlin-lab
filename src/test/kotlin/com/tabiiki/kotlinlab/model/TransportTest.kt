7e837f4850768a71b12b36a44facb19da25bbb0f
tim <wright.tim.j@gmail.com>
2022-03-24 10:33:37 +0000

    refactor test due to hold loop

--- src/test/kotlin/com/tabiiki/kotlinlab/model/TransportTest.kt
+++ src/test/kotlin/com/tabiiki/kotlinlab/model/TransportTest.kt
@@ -2,6 +2,9 @@ package com.tabiiki.kotlinlab.model
 
 import com.tabiiki.kotlinlab.configuration.StationConfig
 import com.tabiiki.kotlinlab.configuration.TransportConfig
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -9,15 +12,16 @@ import org.junit.jupiter.api.Test
 internal class TransportTest {
 
     @Test
-    fun `train moving between Stratford and West Ham stations`() = runBlocking<Unit> {
+    fun `train moving between Stratford and West Ham stations`() = runBlocking{
 
         val stratford = Pair(51.541692575874, -0.00375164102719075)
         val westHam = Pair(51.528525530727, 0.00531739383278791)
         val northGreenwich = Pair(51.628525530727, 0.00531739383278791)
 
         val train = Transport(TransportConfig(transportId = 1, capacity = 10, power = 1, weight = 20, topSpeed = 75))
+        train.linePosition = Pair("1", "2")
 
-        train.depart(
+        val res = async{ train.depart(
             from = Station(
                 StationConfig(id = "1", latitude = stratford.first, longitude = stratford.second), listOf()
             ),
@@ -25,11 +29,18 @@ internal class TransportTest {
                 StationConfig(id = "2", latitude = westHam.first, longitude = westHam.second), listOf()
             ),
             next = Station(
-                StationConfig(id = "2", latitude = northGreenwich.first, longitude = northGreenwich.second), listOf()
+                StationConfig(id = "3", latitude = northGreenwich.first, longitude = northGreenwich.second), listOf()
             ),
-        )
+        ) }
 
-        assertThat(train.physics.distance).isEqualTo(0.0)
+        do {
+            delay(1000)
+        }while (!train.isStationary())
+
+        assertThat(train.linePosition.first).isEqualTo("2")
+        assertThat(train.linePosition.second).isEqualTo("3")
+
+        res.cancelAndJoin()
     }
 
 }
