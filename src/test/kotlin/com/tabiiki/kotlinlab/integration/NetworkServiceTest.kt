48c81b2c28fbebcb1db42217badfa1130c7e02ae
tim <wright.tim.j@gmail.com>
2022-05-14 20:04:20 +0100

    stupid actions

--- src/test/kotlin/com/tabiiki/kotlinlab/integration/NetworkServiceTest.kt
+++ src/test/kotlin/com/tabiiki/kotlinlab/integration/NetworkServiceTest.kt
@@ -14,7 +14,7 @@ import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 
 //@Disabled
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 @ActiveProfiles("test")
 @SpringBootTest
 class NetworkServiceTest @Autowired constructor(
